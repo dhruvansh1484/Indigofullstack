b0d1701710be61c20aff6ef02a5586cadc41d552
dhruvansh1484 <66269967+dhruvansh1484@users.noreply.github.com>
2024-07-29 20:11:40 +0530

    Update flightDetails.js

--- server/routes/flightDetails.js
+++ server/routes/flightDetails.js
@@ -12,17 +12,17 @@ router.get("/flights", async (req, res) => {
 });
 
 // get by id
-// router.get("/flights/:flight_id", async (req, res) => {
-//   const flightId = req.params.flight_id;
-//   try {
-//     const flight = await FlightDetailModel.findOne({ flight_id: flightId });
-//     if (!flight) {
-//       return res.status(404).send({ message: "flight not found" });
-//     }
-//     res.json({ flight });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+router.get("/flights/:flight_id", async (req, res) => {
+  const flightId = req.params.flight_id;
+  try {
+    const flight = await FlightDetailModel.findOne({ flight_id: flightId });
+    if (!flight) {
+      return res.status(404).send({ message: "flight not found" });
+    }
+    res.json({ flight });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
